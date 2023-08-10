02cfd15ae7ad39d9155d5eb63e53d3cb0d128672
Syahroni Bawafi <syahronibwf@gmail.com>
2023-08-10 15:30:33 +0700

    section trending and title

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -4,8 +4,8 @@ const Trending = () => {
   return (
     <section className="w-full overflow-hidden my-16">
       <div className="w-full px-3">
-        <div>
-          <h1 className="text-2xl font-semibold tracking-wider">Trending</h1>
+        <div className="relative before:content-[''] before:absolute before:block before:w-1 before:h-full before:bg-teal before:rounded-full">
+          <h1 className="ml-3 text-2xl font-semibold tracking-wider">Trending</h1>
         </div>
       </div>
     </section>
