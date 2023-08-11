33bd6ce2957d205ba345f7772c9c50f0b3085842
Syahroni Bawafi <syahronibwf@gmail.com>
2023-08-11 07:10:57 +0700

    parsing data trending on card trending

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,8 +1,13 @@
+"use client";
+import { IMAGE_URL } from "@/api/apiConfig";
+import { ContextApi } from "@/context/tmdbAPI";
 import Image from "next/image";
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
 
 const Trending = () => {
+  const { trendings } = ContextApi();
+
   return (
     <section className="w-full overflow-hidden my-16 mx-3">
       <div className="w-full">
@@ -16,18 +21,24 @@ const Trending = () => {
         </div>
         <p className="mt-1">Trending Movie Just For You</p>
       </div>
-      <div className="w-[16%] h-[350px] mt-6 rounded overflow-hidden bg-black">
-        <div className="relative w-full h-4/6">
-          <Image src={"https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg"} alt="asd" fill />
-        </div>
-        <div className="mx-2 my-2">
-          <div className="flex items-center gap-1">
-            <AiFillStar className="text-lg text-teal" />
-            <p>7.5</p>
+      <div className="flex gap-5 w-full my-6 overflow-x-scroll">
+        {trendings.map((item, index: number) => (
+          <div key={index} className="mb-10 min-w-[16%] min-h-[350px] rounded-md overflow-hidden bg-black shadow">
+            <div className="relative w-full h-4/6">
+              <Image src={`${IMAGE_URL}${item.poster_path}`} alt={item.title} fill />
+            </div>
+            <div className="mx-2 my-2">
+              <div className="flex items-center gap-1">
+                <AiFillStar className="text-lg text-teal" />
+                <p className="tracking-widest">{item.vote_average.toFixed(1)}</p>
+              </div>
+              <div className="h-[40px]">
+                <h1 className="mt-1 text-lg font-semibold line-clamp-2 leading-5">{item.original_title}</h1>
+              </div>
+              <p className="mt-3 text-sm font-light">32-22-2003</p>
+            </div>
           </div>
-          <h1 className="mt-1 text-lg font-semibold line-clamp-2">Oppenheimer</h1>
-          <p className="mt-3 text-sm font-light">32-22-2003</p>
-        </div>
+        ))}
       </div>
     </section>
   );
