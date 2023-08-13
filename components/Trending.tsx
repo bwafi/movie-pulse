978d386fc66bbc5931e1c29d17d9cc36aea28dfd
Syahroni Bawafi <syahronibwf@gmail.com>
2023-08-13 16:02:53 +0700

    change title

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { ContextApi } from "@/context/tmdbAPI";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import Card from "./ui/Card";
-import { CSSTransition, SwitchTransition } from "react-transition-group";
 import AnimationCard from "./ui/AnimationCard";
 
 const Trending = () => {
   const { trendingMovies } = ContextApi();
   const [currentTrending, setCurrentTrending] = useState<"day" | "week">("day");
-  const nodeRef = useRef(null);
 
   const handleTrendingDay = () => {
     setCurrentTrending("day");
@@ -22,13 +20,13 @@ const Trending = () => {
     <section className="w-full my-16 mx-3">
       <div className="w-full">
         <div className="relative flex items-center gap-5 before:content-[''] before:absolute before:block before:w-1 before:h-3/4 before:bg-teal before:rounded-full">
-          <h1 className="ml-3 text-2xl font-semibold ">Trending</h1>
+          <h1 className="ml-3 text-2xl font-semibold ">Trending Movies</h1>
           <div className="relative border border-teal rounded-full text-sm z-[1]">
             <button onClick={handleTrendingDay} className="py-0.5 px-5 ">
-              Day
+              Today
             </button>
             <button onClick={handleTrendingWeek} className="py-0.5 px-5">
-              Week
+              This Week
             </button>
             <div
               className={`absolute top-0 w-6/12 h-full bg-teal rounded-full z-[-1] transition-transform ${
@@ -36,7 +34,7 @@ const Trending = () => {
               }`}></div>
           </div>
         </div>
-        <p className="mt-1">Trending Movie Just For You</p>
+        <p className="mt-1">Trending Movies Just For You</p>
       </div>
 
       <AnimationCard keyAction={currentTrending}>
