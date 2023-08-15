ac32e1c209c8512987069b5ff5d7b2326e4d659a
Syahroni Bawafi <syahronibwf@gmail.com>
2023-08-15 19:47:02 +0700

    adding file Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="mx-3">
+      <div>&copy;</div>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,15 @@
 import React, { ReactNode } from "react";
 import Sides from "./Sides";
+import Footer from "./Footer";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="relative grid grid-cols-12 w-full mx-auto">
       <Sides />
-      <div className="col-start-3 col-end-13 row-start-[-1] relative">{children}</div>
+      <div className="col-start-3 col-end-13 row-start-[-1] relative">
+        <main>{children}</main>
+        <Footer />
+      </div>
     </div>
   );
 };
