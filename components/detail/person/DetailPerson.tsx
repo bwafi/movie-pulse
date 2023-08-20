51b2f865f449fef65064a112cdb3c312a8c72682
Syahroni Bawafi <syahronibwf@gmail.com>
2023-08-20 16:07:23 +0700

    read more logic

--- components/detail/person/DetailPerson.tsx
+++ components/detail/person/DetailPerson.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { GoChevronRight } from "react-icons/go";
 
 interface DetailPersonProps {
@@ -7,6 +8,8 @@ interface DetailPersonProps {
 }
 
 const DetailPerson = ({ name, biography }: DetailPersonProps) => {
+  const [showParagraph, setShowParagraph] = useState<boolean>(false);
+
   const paragraphs = biography?.split("\n\n");
 
   return (
@@ -15,19 +18,24 @@ const DetailPerson = ({ name, biography }: DetailPersonProps) => {
       <div className="mt-5">
         <h2 className="font-semibold text-white text-2xl">Biography</h2>
         <div className="relative">
-          <div className="relative max-h-[222px] overflow-hidden after:content-[''] after:block after:absolute after:w-full after:h-[22px] after:bg-gradient-to-r after:from-white/0 after:to-green-black after:bottom-0">
+          <div
+            className={`relative overflow-hidden after:content-[''] after:block after:absolute after:w-full after:h-[22px] after:bg-gradient-to-r after:from-white/0 after:to-green-black after:bottom-0 ${
+              showParagraph ? "after:h-0" : "max-h-[222px]"
+            }`}>
             {paragraphs?.map((paragraph, index: number) => (
               <p key={index} className="mt-3 leading-[22px] tracking-wide ">
                 {paragraph}
               </p>
             ))}
           </div>
-          <div className="absolute right-0 bottom-[0.5px]">
-            <button className="text-teal flex items-center">
-              Read More
-              <GoChevronRight className="text-teal text-xl" />
-            </button>
-          </div>
+          {!showParagraph && (
+            <div className="absolute right-0 bottom-[0.5px]">
+              <button onClick={() => setShowParagraph(true)} className="text-teal flex items-center">
+                Read More
+                <GoChevronRight className="text-teal text-xl" />
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </section>
