db6bab382276b8678bb31001565ae16182d7d714
Syahroni Bawafi <syahronibwf@gmail.com>
2023-08-23 11:10:48 +0700

    adding folder top-rated

--- app/tv/on-the-air/page.tsx
+++ app/tv/on-the-air/page.tsx
@@ -1,11 +1,26 @@
 "use client";
+import { getList } from "@/api/apiCall";
 import Layout from "@/components/Layout";
-import React from "react";
+import ListPage from "@/components/ui/ListPage";
+import { ApiPorps } from "@/libs/type";
+import React, { useEffect, useState } from "react";
 
 const AiringToday = () => {
+  const [onAirData, setOnAirData] = useState<ApiPorps[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  useEffect(() => {
+    getList("tv", "on_the_air", currentPage).then((res) => {
+      const newData = res.data.results;
+      currentPage === 1 ? setOnAirData(newData) : setOnAirData((prevData) => [...prevData, ...newData]);
+    });
+  }, [currentPage]);
+
   return (
     <div>
-      <Layout>sd</Layout>
+      <Layout>
+        <ListPage titlePage="Currently Airing TV Shows" listData={onAirData} />
+      </Layout>
     </div>
   );
 };
