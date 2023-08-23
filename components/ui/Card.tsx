667e7c8c0fc03481e9bf96739d4cb37a792fce83
Syahroni Bawafi <syahronibwf@gmail.com>
2023-08-23 08:57:07 +0700

    fix link on Card Component

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -8,7 +8,7 @@ const Card = ({ item }: { item: any }) => {
   return (
     <div className="mb-10 min-w-[17%] min-h-[350px] rounded-md overflow-hidden bg-black shadow-md border border-teal/30 hover:border-white/30 transition-colors duration-300">
       <Link
-        href={`${item.media_type === "movie" ? `/movie/detail/${item.id}` : `/tv/detail/${item.id}`}`}
+        href={`${item.title ? `/movie/detail/${item.id}` : `/tv/detail/${item.id}`}`}
         className="relative block w-full h-[280px]">
         <Image src={`${IMAGE_URL}${item.poster_path}`} alt={item.title || item.name} fill />
       </Link>
