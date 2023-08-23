6ae57cf81757633b85b3f433e6c756df991912a1
Syahroni Bawafi <syahronibwf@gmail.com>
2023-08-23 10:17:54 +0700

    adding top-rated folder

---
+++ app/movie/top-rated/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Layout from "@/components/Layout";
+import React from "react";
+
+const TopRated = () => {
+  return (
+    <div>
+      <Layout>sdfsdfj</Layout>
+    </div>
+  );
+};
+
+export default TopRated;
