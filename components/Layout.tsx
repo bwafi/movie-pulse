868dd16f25f69a9607818142b1b06a86908bcf6d
Syahroni Bawafi <syahronibwf@gmail.com>
2023-08-24 11:56:14 +0700

    slider responsive mobile

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,12 +5,12 @@ import Footer from "./Footer";
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex relative w-full">
-      <div className="w-[17%] fixed top-0 left-0 z-40">
+      <div className="w-[17%] hidden lg:block fixed top-0 left-0 z-40">
         <Sides />
       </div>
-      <div className="ml-[17%] w-[83%]">
+      <div className="ml-0 lg:ml-[17%] w-full lg:w-[83%]">
         <main>{children}</main>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </div>
   );
--- components/ui/SliderHero.tsx
+++ components/ui/SliderHero.tsx
@@ -22,27 +22,27 @@ const SliderHero = () => {
         delay: 5000,
         disableOnInteraction: false,
       }}
-      navigation
+      // navigation
       slidesPerView={1}>
       {discovers.slice(0, 10).map((item, index: number) => (
         <SwiperSlide key={index}>
           <div className="w-full">
             <Link
               href={`/movie/detail/${item.id}`}
-              className="w-full block h-[480px] gradient-img relative before:content-[''] before:absolute before:block before:w-full before:h-full before:bg-black/40 before:z-10 ">
+              className="w-full block h-[240px] lg:h-[480px] gradient-img relative before:content-[''] before:absolute before:block before:w-full before:h-full before:bg-black/40 before:z-10 ">
               <Image
                 src={`${IMAGE_URL}${item.backdrop_path}`}
-                alt="sds"
+                alt={item.title}
                 priority
                 fill
                 className="object-cover align-bottom bg-soft-black"
               />
             </Link>
 
-            <div className="relative h-[245px] bottom-32 left-16 z-20">
+            <div className="relative h-[135px] lg:h-[245px] bottom-10 lg:bottom-32 left-0 lg:left-16 z-20">
               <Link
                 href={`/movie/detail/${item.id}`}
-                className="absolute block w-[165px] h-[245px] border border-teal/80 rounded overflow-hidden">
+                className="absolute block w-[90px] lg:w-[165px] h-[135px] lg:h-[245px] border border-teal/80 rounded overflow-hidden">
                 <Image
                   src={`${IMAGE_URL}${item.poster_path}`}
                   alt={item.title}
@@ -51,13 +51,15 @@ const SliderHero = () => {
                   className="shadow-[0px_0px_12px_3px_rgba(0,0,0,0.57)]"
                 />
               </Link>
-              <div className="absolute w-fit top-32 left-48 before:content=[''] before:absolute before:block before:w-3 before:h-full before:bg-teal before:rounded-sm">
-                <div className="ml-6">
-                  <p className="text-sm text-teal">Release : {dayjs(item.release_date).format("MMM DD, YYYY")}</p>
-                  <h1 className="text-[34px] font-semibold text-white">
+              <div className="absolute w-fit top-7 lg:top-32 left-[100px] lg:left-48 before:content=[''] before:absolute before:block before:w-3 before:h-full before:bg-teal before:rounded-sm">
+                <div className="ml-4 lg:ml-6">
+                  <p className="text-xs lg:text-sm text-teal">
+                    Release : {dayjs(item.release_date).format("MMM DD, YYYY")}
+                  </p>
+                  <h1 className="text-base lg:leading-10 lg:text-[34px] font-semibold text-white">
                     <Link href={`/movie/detail/${item.id}`}>{item.title}</Link>
                   </h1>
-                  <span className="w-8 h-8 flex justify-center items-center text-xs font-semibold rounded-full border-[3px] border-teal my-1">
+                  <span className="w-7 lg:w-8 h-7 lg:h-8 flex justify-center items-center text-xs font-semibold rounded-full border-[3px] border-teal my-1">
                     {item.vote_average}
                   </span>
                 </div>
